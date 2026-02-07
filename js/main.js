82e34130cab214d265bed10efbc5d0e3e6316163
Ricardo-19-hue <enriquevalerosales190@gmail.com>
2026-02-07 10:01:14 -0600

    Mensaje de lo que se realizo

--- js/main.js
+++ js/main.js
@@ -1,24 +1,31 @@
 function enviarMensaje(){
-
-    const nombre = document.getElementById("nombre").value
-    const correo = document.getElementById("correo").value
-    const mensaje = document.getElementById("mensaje").value
+    const nombre = document.getElementById("nombre")
+    const correo = document.getElementById("correo")
+    const mensaje = document.getElementById("mensaje")
 
     const datos_validados = validarDatos(nombre) && validarDatos(correo) && validarDatos(mensaje)
 
+
     if (datos_validados == true){
-        alert("Los datos se ingresaron de manera correcta");
-    }else
-        alert("Por favor, ingrese los datos")
-    return false;
+        alert("Los datos se ingresaron de manera correcta.");
+    }else{
+        alert("Por favor, Ingrese todos los datos solicitados");
+    }
+    
+    return false
+} 
+
+function validarDatos(elemento){
+    let validacion = false;
+    let valor_elemento = elemento.value;
+    if(valor_elemento != "" && valor_elemento != null){
+        validacion = true;
+    }else{
+        mostrarErro(elemento);
+    }
+    return validacion
 }
 
-function validarDatos(valor){
-    let validacion = false;
-    if(valor != "" && valor != null){
-        validacion = true;
-
-    }
-
-    return validacion
+function mostrarErro(elemento){
+    elemento.classList.add('input-error');
 }
